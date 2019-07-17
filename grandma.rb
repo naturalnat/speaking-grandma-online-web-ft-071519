6ea6cdab42a562be29f618e8fea886a9cbd0bb55
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 16:44:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase, grandma)
+  if phrase == phrase.uppercase
+  puts "NO, NOT SINCE 1938"
+  end 
+end
+  
