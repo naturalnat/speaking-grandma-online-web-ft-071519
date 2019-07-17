08076ae3499354a0f962be7c3030fb37905fc39d
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 16:44:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,6 +15,5 @@
 def speak_to_grandma(phrase, grandma)
   if phrase == phrase.uppercase
   puts "NO, NOT SINCE 1938"
-  end 
+  end
 end
-  
